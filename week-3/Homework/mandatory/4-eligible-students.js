6b9aeab61fffa1142c7d4ab1420e3e1b4cc348d6
Francesc Rosas <francescrosasbosque@gmail.com>
2020-03-13 12:34:03 +0000

    Update 4-eligible-students.js

--- week-3/Homework/mandatory/4-eligible-students.js
+++ week-3/Homework/mandatory/4-eligible-students.js
@@ -7,8 +7,16 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function eligibleStudents() {
-
+function eligibleStudents(attendances) {
+  return attendances
+    .filter(studentAttendances => {
+      const attendances = studentAttendances[1];
+      return attendances >= 8;
+    })
+    .map(studentAttendances => {
+      const name = studentAttendances[0];
+      return name;
+    });
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
@@ -20,33 +28,37 @@ const attendances = [
   ["Adam", 7],
   ["Tayoa", 11],
   ["Nina", 10]
-]
+];
 
 function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-  
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-  
-    return true;
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
 }
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
-test("eligibleStudents function works",
-  arraysEqual(
-    eligibleStudents(attendances), ["Ahmed", "Clement", "Tayoa", "Nina"]
-  )
-)
+test(
+  "eligibleStudents function works",
+  arraysEqual(eligibleStudents(attendances), [
+    "Ahmed",
+    "Clement",
+    "Tayoa",
+    "Nina"
+  ])
+);
